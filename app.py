8e3c1aa3e1a8760bfa74f046a007564b6e9668d3
HanaTI-Insert <python@hanafn.com>
2019-01-27 20:54:08 +0900

    proto complate

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
+#!/usr/bin/python
 from flask import Flask, render_template
 from bs4 import BeautifulSoup
 import requests
-
 app = Flask(__name__)
 @app.route("/")
 def main():
 	r = requests.get('https://loahae.com/')
 	soup = BeautifulSoup(r.text, 'html.parser')
 	data = soup.find('div', {'class':'content'})
-	return '<!DOCTYPE html><head><meta charset="utf-8"><link href="https://loahae.com/assets/css/loahae.css" rel="stylesheet"></head><body class="main">%s</body></html>' % str(data)
+	return '<!DOCTYPE html><head><meta charset="utf-8"><link href="https://loahae.com/assets/css/loahae.css" rel="stylesheet"></head><body class="main"></br>%s<meta http-equiv="refresh" content="600; URL=/"></body></html>' % str(data)
 app.run(host='0.0.0.0', debug=True)
