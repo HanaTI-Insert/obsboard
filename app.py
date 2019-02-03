d99ca0a2eaf0dee01fbf1e821b4b4a2b6c3d1c08
HanaTI-Insert <python@hanafn.com>
2019-02-03 15:13:07 +0900

    endy add

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
+#-*- coding: utf-8 -*-
 from flask import Flask, render_template
 from bs4 import BeautifulSoup
-import requests
+import requests, time
 app = Flask(__name__)
 @app.route("/")
 def main():
@@ -9,4 +10,9 @@ def main():
     m = soup.find('div', {'class':'box mari'})
     c = soup.find('div', {'class':'box calendar'})
     return render_template('index.html', mari=unicode(m), calendar=unicode(c))
+@app.route('/endy')
+def endy():
+    t = {0:'루테란', 1:'슈테른', 2:'베른', 3:'페이토', 4:'아트로포스', 5:'로헨델', 6:'루테란', 7:'슈테른', 8:'베른', 9:'페이토', 10:'아트로포스', 11:'로헨델', 12:'루테란', 13:'슈테른', 14:'베른', 15:'페이토', 16:'아트로포스', 17:'로헨델', 18:'루테란', 19:'슈테른', 20:'베른', 21:'페이토', 22:'아트로포스', 23:'로헨델'}
+    return render_template('endy.html', town=unicode(t[time.localtime().tm.hour]))
 app.run(host='0.0.0.0', debug=True)
+
